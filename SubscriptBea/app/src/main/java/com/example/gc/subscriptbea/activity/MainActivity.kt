33ab50fa690346b4dfdc2b7140caa1d336730220
Harshit Modi <harshitmodi1991@gmail.com>
2021-12-01 15:49:51 -0500

    HM
    Login and Signup screen design

--- SubscriptBea/app/src/main/java/com/example/gc/subscriptbea/activity/MainActivity.kt
+++ SubscriptBea/app/src/main/java/com/example/gc/subscriptbea/activity/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.gc.subscriptbea.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.gc.subscriptbea.R
 import com.example.gc.subscriptbea.helpers.HMBaseActivity
 
@@ -9,5 +12,15 @@ class MainActivity : HMBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnLoginAction(view: View) {
+        val intent = Intent(this, SignupActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun btnSignupAction(view: View) {
+        val intent = Intent(this, SignupActivity::class.java)
+        startActivity(intent)
     }
 }
