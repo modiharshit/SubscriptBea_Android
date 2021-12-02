3406d2a5810b6497b475e01d550cb45ad11f44c3
Harshit Modi <harshitmodi1991@gmail.com>
2021-12-02 01:26:59 -0500

    HM Add Subscription
    Add Subscription Activity

---
+++ SubscriptBea/app/src/main/java/com/example/gc/subscriptbea/Activity/AddSubscriptionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gc.subscriptbea.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddSubscriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_subscription)
+    }
+}
