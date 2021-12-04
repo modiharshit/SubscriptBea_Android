78c2c600ac398aebe983225897e378247ceafe3b
Harshit Modi <harshitmodi1991@gmail.com>
2021-12-04 00:23:52 -0500

    HM
    Home Navigation bar

--- SubscriptBea/app/src/main/java/com/example/gc/subscriptbea/Activity/HomeActivity.kt
+++ SubscriptBea/app/src/main/java/com/example/gc/subscriptbea/Activity/HomeActivity.kt
@@ -1,16 +1,23 @@
 package com.example.gc.subscriptbea.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.gc.subscriptbea.R
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gc.subscriptbea.model.ItemsViewModel
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Set Toolbar
+        setSupportActionBar(toolbar)
 
         // getting the recyclerview by its id
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
@@ -33,5 +40,22 @@ class HomeActivity : AppCompatActivity() {
         // Setting the Adapter with the recyclerview
         recyclerview.adapter = adapter
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.navbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var itemView = item.itemId
+        when(itemView){
+
+            R.id.profile -> {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return false
     }
 }
